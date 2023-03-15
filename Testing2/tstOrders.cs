1be83d83b56dea6f4aa64805908c347e838b0bc6
Cesar Camacho Vieira <P2653383@my365.dmu.ac.uk>
2023-03-15 19:02:45 +0000

    Added testing for address class

---
+++ Testing2/tstOrders.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing2
+{
+    [TestClass]
+    public class tstOrders
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create
+            clsOrders AnOrders = new clsOrders();
+            //test to see that exists
+            Assert.IsNotNull(AnOrders);
+
+        }
+    }
+}
