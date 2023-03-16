0c79325fc593298b3e28fdf7590b9cf6084980a8
Cesar Camacho Vieira <P2653383@my365.dmu.ac.uk>
2023-03-16 00:21:04 +0000

    Adding a Simple Presentation Layer(tiding up and linking the middle layer)

--- AdminSystem/OrderViewer.aspx.cs
+++ AdminSystem/OrderViewer.aspx.cs
@@ -4,11 +4,23 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        //create a new instance of clsOrders
+        clsOrders AnOrders = new clsOrders();
+        //get the data from the session object
+        AnOrders = (clsOrders)Session["AnOrders"];
+        //display the delivery type for this entry
+        Response.Write(AnOrders.DeliveryType);
+        Response.Write(AnOrders.OrderId);
+        Response.Write(AnOrders.ProductId);
+        Response.Write(AnOrders.QuantityNo);
+        Response.Write(AnOrders.Completed);
+        Response.Write(AnOrders.Date);
 
     }
 }
